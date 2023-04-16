7cb1866303190d2e63ce4cfca56c415a3cdff9c7
Alberto Tirla <albertotirla@gmail.com>
2023-04-16 04:12:31 +0300

    add the irc crate and gitignore modifications
    * add configurability with features of the irc crate
    * if the configuration file is not found, the user will be prompted for details such as nickname, server address and channels list
    * made gitignore ignore the config.toml file
    * this version finally does connect to a channel, so we're going forward

--- src/main.rs
+++ src/main.rs
@@ -1,124 +1,89 @@
-use std::io::BufRead;
+use futures::prelude::*;
+use irc::client::prelude::*;
+use serde::{Deserialize, Serialize};
+use std::{fs, io::stdin, path::Path};
+use toml;
 
-use tokio::io::{AsyncBufRead, AsyncBufReadExt, AsyncWrite, AsyncWriteExt, BufReader, BufWriter};
-use tokio::net::TcpStream;
-use tokio::sync::mpsc::{channel, Receiver, Sender};
-
-use tokio::task;
-
-#[derive(Debug, Clone)]
-struct Server(String);
-#[derive(Debug, Clone)]
-struct Port(u16);
-#[derive(Debug, Clone)]
-struct Nickname(String);
-#[derive(Debug, Clone)]
-struct Channel(String);
-
-#[derive(Debug, Clone)]
-enum Message {
-    Raw(String),
-    Join(Channel),
-    Pong(String),
+#[derive(Debug, Deserialize, Serialize)]
+struct AppConfig {
+    nickname: String,
+    username: Option<String>,
+    realname: Option<String>,
+    server: String,
+    port: Option<u16>,
+    use_tls: Option<bool>,
+    channels: Vec<String>,
 }
 
-async fn read_input(tx: Sender<Message>) {
-    let stdin = tokio::io::stdin();
-    let mut stdin_reader = BufReader::new(stdin);
-    loop {
-        let mut buffer = String::new();
-        match stdin_reader.read_line(&mut buffer).await {
-            Ok(_) => {
-                let trimmed_buffer = buffer.trim();
-                let message = if trimmed_buffer.starts_with("JOIN ") {
-                    Message::Join(Channel(trimmed_buffer[5..].trim().to_string()))
-                } else {
-                    Message::Raw(trimmed_buffer.to_string())
-                };
-                tx.send(message).await.unwrap();
-            }
-            Err(_) => break,
-        }
-    }
-}
-async fn read_stream<R: AsyncBufRead + Unpin>(mut stream: R, tx: Sender<Message>) {
-    loop {
-        let mut buffer = String::new();
-        match stream.read_line(&mut buffer).await {
-            Ok(_) => {
-                println!("Received line from server: {}", buffer); // Add this line for logging
-                let trimmed_buffer = buffer.trim();
-                let message = if trimmed_buffer.starts_with("PING") {
-                    Message::Pong(trimmed_buffer[5..].trim().to_string())
-                } else {
-                    Message::Raw(trimmed_buffer.to_string())
-                };
-                tx.send(message).await.unwrap();
-            }
-            Err(_) => break,
-        }
+async fn read_config() -> AppConfig {
+    if Path::new("config.toml").exists() {
+        let config_str = fs::read_to_string("config.toml").expect("Unable to read config.toml");
+        toml::from_str(&config_str).expect("Unable to parse config.toml")
+    } else {
+        println!("configuration file not found!");
+        println!("In the following prompts, you'll be asked to fill in the required information about yourself and your irc network, in order to connect to your server");
+        let mut nickname = String::new();
+        let mut server = String::new();
+        let mut channels_str = String::new();
+
+        println!("Type your nickname, then press enter: ");
+        stdin().read_line(&mut nickname).unwrap();
+        println!("Type the address of your irc network. This is the domain one connects to specifically with an irc client, for example `irc.libera.chat`: ");
+        stdin().read_line(&mut server).unwrap();
+        println!("Optionally, type in a list of channels you want to be prejoined to on startup, comma sepparated");
+        stdin().read_line(&mut channels_str).unwrap();
+
+        println!("configuration complete!");
+        let channels = channels_str
+            .trim()
+            .split(',')
+            .map(|channel| channel.trim().to_string())
+            .collect();
+
+        let config = AppConfig {
+            nickname: nickname.trim().to_string(),
+            username: None,
+            realname: None,
+            server: server.trim().to_string(),
+            port: None,
+            use_tls: None,
+            channels,
+        };
+
+        let config_str = toml::to_string(&config).expect("Unable to serialize config");
+        fs::write("config.toml", config_str).expect("Unable to write config.toml");
+
+        config
     }
 }
 
-async fn write_stream<W: AsyncWrite + Unpin>(mut stream: W, mut rx: Receiver<Message>) {
-    while let Some(message) = rx.recv().await {
-        match message {
-            Message::Join(channel) => {
-                let join_message = format!("JOIN {}\r\n", channel.0);
-                println!("Sending join message: {}", join_message); // Add this line for logging
-                stream.write_all(join_message.as_bytes()).await.unwrap();
-            }
-            Message::Raw(raw_message) => {
-                let raw_message = format!("{}\r\n", raw_message);
-                println!("Sending raw message: {}", raw_message); // Add this line for logging
-                stream.write_all(raw_message.as_bytes()).await.unwrap();
-            }
-            Message::Pong(server) => {
-                let pong_message = format!("PONG {}\r\n", server);
-                println!("Sending pong message: {}", pong_message); // Add this line for logging
-                stream.write_all(pong_message.as_bytes()).await.unwrap();
-            }
-        }
-    }
-}
 #[tokio::main]
-async fn main() {
-    let (_tx_input, _rx_input) = channel::<Message>(32);
-    let (tx_stream, rx_stream) = channel::<Message>(32);
-    let tx_stream_read = tx_stream.clone();
-    let tx_stream_input = tx_stream.clone();
-    println!("Enter server address:");
-    let server = read_line();
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let config = read_config().await;
 
-    println!("Enter server port (default: 6667):");
-    let port = read_line().parse::<u16>().unwrap_or(6667);
+    let irc_config = Config {
+        nickname: Some(config.nickname),
+        username: config.username,
+        realname: config.realname,
+        server: Some(config.server),
+        port: config.port,
+        use_tls: config.use_tls,
+        channels: config.channels,
+        ..Config::default()
+    };
 
-    println!("Enter your nickname:");
-    let nickname = read_line();
+    let mut client = Client::from_config(irc_config).await?;
+    client.identify()?;
+    let mut stream = client.stream()?;
 
-    let stream = TcpStream::connect(format!("{}:{}", server, port))
-        .await
-        .unwrap();
-
-    let (stream_reader, mut stream_writer) = stream.into_split();
-    stream_writer
-        .write_all(format!("NICK {}\r\n", nickname).as_bytes())
-        .await
-        .unwrap();
-    stream_writer
-        .write_all(format!("USER {} 0 * :{}\r\n", nickname, nickname).as_bytes())
-        .await
-        .unwrap();
-    stream_writer.write_all(b"CAP REQ :sasl\r\n").await.unwrap();
-    let read_input_task = task::spawn(read_input(tx_stream_input));
-    let read_stream_task = task::spawn(read_stream(BufReader::new(stream_reader), tx_stream_read));
-    let write_stream_task = task::spawn(write_stream(BufWriter::new(stream_writer), rx_stream));
-    tokio::try_join!(read_input_task, read_stream_task, write_stream_task).unwrap();
-}
-fn read_line() -> String {
-    let mut input = String::new();
-    let stdin = std::io::stdin();
-    let mut stdin_lock = stdin.lock();
-    stdin_lock.read_line(&mut input).unwrap();
-    input.trim().to_string()
+    while let Some(message) = stream.next().await.transpose()? {
+        print!("{}", message);
+        match message.command {
+            Command::PRIVMSG(ref target, ref msg) => {
+                println!("{}: {}", target, msg);
+            }
+            _ => {}
+        }
+    }
+    Ok(())
 }
